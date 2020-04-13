716fb89af38b5e07ed326b61d66ef4207359c8c3
AudiantoFari <minecraftindo0@gmail.com>
2020-04-13 17:41:28 +0700

    Initialize Project

---
+++ application/controllers/Home.php
@@ -0,0 +1,9 @@
+<?php 
+
+class Home extends CI_Controller {
+    public function index() {
+        $this->load->view('inc/header');
+        $this->load->view('home/index');
+        $this->load->view('inc/footer');
+    }
+}
